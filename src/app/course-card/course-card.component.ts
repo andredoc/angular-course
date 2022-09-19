2c0923e5246a8a8bd1d88276f22bed2c166c06ea
andredoc <andredoc@MBP-de-Andre.home>
2022-09-19 10:25:10 +0200

    use ngSwitch to identify type of category course

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Course } from '../model/course'
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent implements OnInit {
+
+  @Input() 
+  course: Course;
+
+  @Input()
+  cardIndex: number;
+
+  @Output()
+  courseSelected = new EventEmitter<Course>()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  isImageVisible() {
+    return this.course && this.course.iconUrl;
+  }
+
+  onCourseViewed() {
+    this.courseSelected.emit(this.course)
+  }
+
+  cardClasses() {
+    if(this.course.category === 'BEGINNER') {
+      return 'beginner';
+    }
+  }
+
+  cardStyles() {
+    return { 
+      'background-image': 'url(' + this.course.iconUrl + ')'
+    };
+  }
+
+}
